6d107a1115d25d9e038d8232d2a49de7b58bc4d4
BlondGT <112488079+BlondGT@users.noreply.github.com>
2023-01-28 14:26:53 +0200

    Changed project

---
+++ src/test/main/Java/ReverseTest.java
@@ -0,0 +1,13 @@
+package main.Java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseTest {
+
+    @Test
+    void testReverse() {
+        assertEquals("opol", "opol", "lopo");
+    }
+}
